9ec28fb334b112d380c9d36054ab9a5ec6f0c410
eacdev <>
2020-07-22 18:14:05 -0400

    Generate hex and sequences.

--- src/components/CharacterSequence.js
+++ src/components/CharacterSequence.js
@@ -3,21 +3,28 @@ import React from "react";
 import Character from "./Character";
 
 function CharacterSequence({ sequence }) {
-  // Ex: ["0xF123", "^.SATIN:-;|^"]
-  const split = sequence[1].split(/([A-Z]+)/);
+  // Ex: "0xEF8B ^%PRESSURE!%"
+  let split = sequence.split(" ");
 
-  const output = split.map(w => {
-    // If w has a letter in [A-Z] then it is a word, we pass it to Character componenent.
-    if ([...w][0].charAt(0).match(/[A-Z]/)) {
+  // "0xEF8B"
+  const hex = split[0];
+
+  // ["^%", "PRESSURE", "!%"]
+  const characters = split[1].split(/([A-Z]+)/)
+
+  const output = characters.map(w => {
+    // If w has a letter in [A-Z] then it the word in the sequence, we pass it to Character componenent.
+    if ([...w][0] && [...w][0].charAt(0).match(/[A-Z]/)) {
       return <Character character={w}></Character>;
     }
+
     // w is not a word, each of it's letters gets it's own Character component.
     return [...w].map(c => <Character character={c}></Character>);
 })
 
   return (
     <p>
-      {sequence[0]} {output}
+      {hex} {output}
     </p>
   );
 }
